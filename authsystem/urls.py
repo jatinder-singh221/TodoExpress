90b98670bd311d8ee9fc193ffedb71715e9b9cc8
jatinder-singh221 <jatindersing19@gmail.com>
2021-03-13 18:53:24 +0530

    modified ui of the login

--- authsystem/urls.py
+++ authsystem/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import login_user,register_user,forget_password_user
+from .views import login_user,register_user,forget_password_user,test
 
 urlpatterns = [
     path('login/', login_user, name = 'Login'),
     path('register/', register_user, name = 'Register'),
     path('password/', forget_password_user, name = 'Password'),
+    path('test/', test, name = 'Test')
 ]
 
 app_name = 'authsystem'
--- authsystem/views.py
+++ authsystem/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm,PasswordChangeForm
+
 
 # login of user
 def login_user(request):
 
-    if request.method == 'GET':
-        login_form = AuthenticationForm()
+    if request.method == 'POST':
+        post_returned_form = AuthenticationForm(request = request, data = request.POST)
 
-    elif request.method == 'POST':
-        print('hi')
+        # checking form 
 
+        if post_returned_form.is_valid():
+            get_username = post_returned_form.cleaned_data.get('username')
+            get_password = post_returned_form.cleaned_data.get('password')
+            let_auth = authenticate(username = get_username, password = get_password)
+
+            # if user is valid then login
+            if let_auth is not  None:
+                login(request, let_auth)
+                return redirect('auth:Test')
+            
+            # if fail
+            else :
+                messages.error(request, 'Check Username Or Password')
+                
+        # if not valid
+        else:
+            messages.error(request, 'Check Username Or Password')
+
+    login_form = AuthenticationForm()
     context = {'html_login_form': login_form}
     return render(request, 'login.html',context)
+
 
 # Register of User
 def register_user(request):
@@ -36,3 +58,9 @@ def forget_password_user(request):
 
     context = {'html_resetpassword_form': passwordchange_form}
     return render(request,'password.html', context)
+
+
+# for testing
+
+def test(request):
+    return render(request, 'test.html')
