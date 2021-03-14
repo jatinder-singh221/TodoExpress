049000e078734473cc805c4c9d2e3d036eade7a8
jatinder-singh221 <jatindersing19@gmail.com>
2021-03-14 23:45:48 +0530

    extended user model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import extendexd_user_model
 
-# Register your models here.
+
+@admin.register(extendexd_user_model)
+class extended_user_admin(admin.ModelAdmin):
+    pass
--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class extendexd_user_model(models.Model):
+    linked_user = models.OneToOneField(User, on_delete = models.CASCADE, verbose_name = 'User', db_column = 'linked_user')
+    user_profile = models.ImageField('User Image', upload_to= 'userprofiles', db_column = 'user_profile')
+
+    # manage name   
+    extended_user = models.Manager()
+
+    class Meta:
+        db_table = 'extended_user'
+        verbose_name_plural = 'Extended users'
